c61256612451db89a54e0938e22b5ce94df217e9
gchaewon <conjs1@naver.com>
2023-09-26 23:59:44 +0900

    [05_우선순위큐] 9월 26일 -Update

--- 05_우선순위큐/2075.cpp
+++ 05_우선순위큐/2075.cpp
@@ -7,20 +7,19 @@ int main() {
     int t, num;
     priority_queue<int, vector<int>, greater<int>>
         answer; // 오름차순으로 저장하는 우선 순위 큐
-    cin >> t;
-
     // 입출력 속도 향상
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+    cin >> t;
+
     // N X N 입력 받기
-    for (int i = 0; i < t; i++) {
-        for (int j = 0; j < t; j++) {
-            cin >> num;
-            answer.push(num);
-            // t번째로 큰 수까지만 저장
-            if (answer.size() > t)
-                answer.pop();
+    for (int i = 0; i < t * t; i++) {
+        cin >> num;
+        answer.push(num);
+        // t번째로 큰 수까지만 저장
+        if (answer.size() > t) {
+            answer.pop();
         }
     }
     // 오름차순으로 입력했으므로 top값 (t번째로 큰 수) 반환
